baed86e6f3f2e9c9821b1c332170e454e2649ab1
Nurbek <161306161+nurbek2107@users.noreply.github.com>
2024-05-30 21:36:18 +0500

    main
    Signed-off-by: Nurbek <161306161+nurbek2107@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [showConent, setshowConent] = useState (true);
+  const [names, setNames] = useState([
+    { id: 1, name: 'Nurbek' },
+    { id: 2, name: 'Tillo' },
+    { id: 3, name: 'Bekzod' },
+  ]);
+  const handleDelete = (id) => {
+    const filteredNames = names.filter((name) => {
+    return  name.id !== id
+    });
+    setNames(filteredNames);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="./logo.svg" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {  showConent   && <button onClick={() => setshowConent(false)}>Hide Connet</button>}
+      {  !showConent  &&  <button onClick={() => setshowConent(true)}>show Connet</button>}
+    
+      {showConent &&
+        <div>
+          {names.length === 0 && <h3> not connet yet  :(</h3> }
+          {
+            names.map((name) => (
+              <div key={name.id}>
+                <h1>{name.name}</h1>
+                <button onClick={() => handleDelete(name.id)}>delete</button>
+          </div>))
+          }
+        </div>
+      }
     </div>
   );
 }
-
 export default App;
